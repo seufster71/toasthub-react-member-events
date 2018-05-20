328bf6b0c7c8d791b2585d2c497cf55c5c0e1c1c
seufster <seufster@gmail.com>
2018-05-20 06:56:34 -0400

    init

---
+++ events-container.js
@@ -0,0 +1,47 @@
+/*
+* Author Edward Seufert
+*/
+'use-strict';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as appPrefActions from '../../core/common/apppref-actions';
+import fuLogger from '../../core/common/fu-logger';
+import EventsView from '../../memberView/events/events-view';
+
+
+class EventsContainer extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+
+	componentDidMount() {
+		//this.props.actions.initMember();
+	}
+
+  render() {
+			fuLogger.log({level:'TRACE',loc:'EventsContainer::render',msg:"Hi there"});
+      return (
+				<EventsView/>
+			);
+  }
+}
+
+EventsContainer.propTypes = {
+	appPrefs: PropTypes.object,
+	lang: PropTypes.string,
+	appGlobal: PropTypes.object,
+	actions: PropTypes.object
+};
+
+function mapStateToProps(state, ownProps) {
+  return {lang:state.lang, appPrefs:state.appPrefs};
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions:bindActionCreators(appPrefActions,dispatch) };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(EventsContainer);
